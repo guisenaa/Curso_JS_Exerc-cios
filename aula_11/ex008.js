d52bab76ba45680f3bd7a4791fb97a95b6f02594
Guilherme Sena <guisenaasts@gmail.com>
2024-03-21 09:31:48 -0300

    first commit

---
+++ aula_11/ex008.js
@@ -0,0 +1,8 @@
+var vel = 86.8
+console.log(`A velocidade do seu carro é de ${vel} Km/h`)
+
+if (vel > 60){ //CONDIÇÃO SIMPLES
+    console.log('Você ultrapassou o limite de velocidade. MULTADO!')
+}
+
+console.log('Dirija sempre usando cinto de segurança')
